5a73470a8e6a9621981505b73c56bec1e8e28e88
李俊 <lijun@021.com>
2020-11-18 14:46:16 +0800

    更新代码

--- koa_web/src/views/skinPeeler.vue
+++ koa_web/src/views/skinPeeler.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="about">
-    <h1>快来和妲己玩耍吧~</h1>
+    <h1 :style="{color:colorStyle}">快来和妲己玩耍吧~~</h1>
+    当前设置颜色值为：{{colorStyle}}
   </div>
 </template>
 <script>
 export default {
-  name: 'skinPeeler',
-  data(){
+  name: 'skin',
+  data() {
     return {
-
+      colorStyle:'#333'
     }
   },
   created() {
+    var that = this;
     fetch('http://127.0.0.1:3000/home/getColor')
       .then(function (response) {
         return response.json();
       })
-      .then(function (myJson) {
-        console.log(myJson);
+      .then(function (data) {
+        console.log(data.data.value)
+        that.colorStyle = data.data.value
       });
-  },
+  }
 }
 </script>
 
